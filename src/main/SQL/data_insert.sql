0ed2b453da4c172d3a0aedf73f8f1fb3c7bed48b
alexkeung555 <alexkeung555@hotmail.com>
2021-05-08 21:42:14 +0800

    Modifed for multi lang support (Second Commit) -AK

---
+++ src/main/SQL/data_insert.sql
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  alexk
+ * Created: 2021年5月8日
+ */
+
+INSERT INTO Foods(name,description,available,price) VALUES ('Carbonara', 'This Creamy Carbonara recipe is a plate of heavenly, creamy pasta. Silky spaghetti with crispy pancetta in a super creamy and cheesy sauce.',true,100);
+INSERT INTO Foods(name,description,available,price) VALUES ('New York Strip', 'Well marbled, full bodied and rich with beefy flavor, a New York Strip is firm and tender when it comes to texture, flavor and fat. Cut from the shorter side of the beef loin, the muscles in this area do little work, and that spells tenderness every time. At Ruth’s, ours is a USDA Prime cut of short loin that can be enjoyed as a 16oz boneless strip steak or as a 19oz bone-in specialty cut',true,150);
+INSERT INTO Foods(name,description,available,price) VALUES ('Pizza Margherita', 'Thin crust pizza dough topped with a simple raw tomato sauce, fresh mozzarella cheese, and basil.',true,110);
+INSERT INTO Foods(name,description,available,price) VALUES ('Garlic Herb Butter Roast Chicken', 'Roast Chicken slathered with a garlic-herb-lemon butter then oven roasted to golden crispy perfection.',true,200);
+INSERT INTO Foods(name,description,available,price) VALUES ('Seared Crispy Skin Duck Breast With Duck Fat Fried Potatoes', 'A perfectly cooked, pan-fried duck breast will have a crisp, golden skin and succulent pink flesh.',true,80);
+INSERT INTO Foods(name,description,available,price) VALUES ('Hoisin duck stir-fry', 'Looking for a quick midweek meal? It’s low in fat and calories, but full of flavour',true,75);
+INSERT INTO Foods(name,description,available,price) VALUES ('Pan Seared Salmon with Lemon Garlic Butter Sauce', 'Try this roast turkey breast as an tasty journey for your festive dinner',true,100);
+INSERT INTO Foods(name,description,available,price) VALUES ('Duck ragu with pappardelle & swede', 'Golden-crisp on top and just barely cooked in the center.',true,100);
+INSERT INTO Foods(name,description,available,price) VALUES ('Lobster Linguine', 'Linguine with Boston Lobster and Fresh Home-Made Tomato Sauce',true,98);
+INSERT INTO Foods(name,description,available,price) VALUES ('Penne Amatriciana', 'Penne with Amatriciana Sauce, Guanciale, Onion',true,75);
+
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Alex Keung', 2);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Simon Keung', 3);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Anima Keung', 4);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Alex Keung', 5);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very cheap!', 'Simon Keung', 6);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Soe Keung', 7);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Alex Wong', 8);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very cheap!', 'Anima Wong', 9);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Soe Wong', 6);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Alex Keung', 1);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Simon Keung', 2);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Soe Keung', 3);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Jenny Keung', 4);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very cheap!', 'Simon Keung', 8);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Soe Keung', 7);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very expensive!', 'Jenny Wong', 6);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very cheap!', 'Simon Wong', 3);
+INSERT INTO Comment(detail,username,food_id) VALUES('Very tasty!', 'Jenny Wong', 2);
