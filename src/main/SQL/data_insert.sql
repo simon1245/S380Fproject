341119d046d223df81b26b1642f6167ee15bd7a0
simon1245 <37098385+simon1245@users.noreply.github.com>
2021-05-09 20:03:34 +0800

    SQL

--- src/main/SQL/data_insert.sql
+++ src/main/SQL/data_insert.sql
@@ -1,12 +1,74 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  alexk
- * Created: 2021年5月8日
- */
+CREATE TABLE Foods (
+    food_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    available Boolean NOT NULL,
+    price INTEGER NOT NULL,
+    PRIMARY KEY (food_id)
+);
+CREATE TABLE Attachment (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    filename VARCHAR(255) DEFAULT NULL,
+    content_type VARCHAR(255) DEFAULT NULL,
+    content BLOB DEFAULT NULL,
+    food_id INTEGER DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (food_id) REFERENCES Foods(food_id)
+);
+
+CREATE TABLE Comment (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    detail VARCHAR(255) NOT NULL,
+    username VARCHAR(50) DEFAULT NULL,
+    food_id INTEGER DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (food_id) REFERENCES Foods(food_id)
+);
+
+CREATE TABLE Orders (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    username VARCHAR(50) DEFAULT NULL,
+    order_time TIMESTAMP ,
+    Cost INTEGER ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE OrderedFood (
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    name VARCHAR(255) NOT NULL,
+    order_id INTEGER DEFAULT NULL,
+    food_id INTEGER DEFAULT NULL,
+    price INTEGER DEFAULT NULL,
+    quantity INTEGER DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id) REFERENCES Orders(id)
+);
+
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(8) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE user_roles (
+    user_role_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    username VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_role_id),
+    FOREIGN KEY (username) REFERENCES users(username)
+);
+
+INSERT INTO users VALUES ('keith', '{noop}keithpassword', 'keith Y','91234567','Hong Kong');
+INSERT INTO user_roles(username, role) VALUES ('keith', 'ROLE_ADMIN');
+
+INSERT INTO users VALUES ('vanessa', '{noop}vanessapassword', 'vanessa B','61234867','Hong Kong');
+INSERT INTO user_roles(username, role) VALUES ('vanessa', 'ROLE_ADMIN');
+
+INSERT INTO users VALUES ('kevin', '{noop}kevinpassword' , 'kevin H','48137694','Hong Kong');
+INSERT INTO user_roles(username, role) VALUES ('kevin', 'ROLE_USER');
 
 INSERT INTO Foods(name,description,available,price) VALUES ('Carbonara', 'This Creamy Carbonara recipe is a plate of heavenly, creamy pasta. Silky spaghetti with crispy pancetta in a super creamy and cheesy sauce.',true,100);
 INSERT INTO Foods(name,description,available,price) VALUES ('New York Strip', 'Well marbled, full bodied and rich with beefy flavor, a New York Strip is firm and tender when it comes to texture, flavor and fat. Cut from the shorter side of the beef loin, the muscles in this area do little work, and that spells tenderness every time. At Ruth’s, ours is a USDA Prime cut of short loin that can be enjoyed as a 16oz boneless strip steak or as a 19oz bone-in specialty cut',true,150);
